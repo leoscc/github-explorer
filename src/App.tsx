9aa404f6fb93f74e297c958f2ee34eb91e3ae7f2
leon-carvalho <leonardoscc15@gmail.com>
2020-04-20 12:14:24 -0300

    Adding global styles with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyles />
+    </BrowserRouter>
+  </>
 );
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+import githubBackground from '../assets/github-background.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-size: 16px;
+    font-family: Roboto, sans-serif;
+  }
+
+  #root {
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 40px 20px;
+  }
+
+  button, a {
+    cursor: pointer;
+  }
+`;
