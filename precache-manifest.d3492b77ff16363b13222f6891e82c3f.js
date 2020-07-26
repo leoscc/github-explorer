13a147cddc0bb4590713bda9400d857dc606d5d2
leon-carvalho <leonardoscc15@gmail.com>
2020-07-26 12:57:02 -0300

    Updates

---
+++ precache-manifest.d3492b77ff16363b13222f6891e82c3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fe1a583c3c8afbd52271aabf31eb80",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "a6901ef086bda6bb68a4",
+    "url": "/github-explorer/static/js/2.a03cd119.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.a03cd119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e1cd89e9a415823153",
+    "url": "/github-explorer/static/js/main.99a385f3.chunk.js"
+  },
+  {
+    "revision": "b76f610d1055037be066",
+    "url": "/github-explorer/static/js/runtime-main.efd11135.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
